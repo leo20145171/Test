b15af8ec3945e916806898ede276bdef2932a3e7
huofenghuang <huofenghuang@ubuntu>
2014-12-22 17:45:41 +0800

    move file

---
+++ frontend/class/Lottery.class.php
@@ -0,0 +1,25 @@
+<?php
+
+/** 
+ * @author huofenghuang
+ * 
+ */
+class Lottery {
+	// TODO - Insert your code here
+	
+	/**
+	 */
+	function __construct() {
+		
+		// TODO - Insert your code here
+	}
+	
+	/**
+	 */
+	function __destruct() {
+		
+		// TODO - Insert your code here
+	}
+}
+
+?>
