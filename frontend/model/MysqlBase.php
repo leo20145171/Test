db4b227c8ecc3b7c89ad0a94b610784498b79800
huofenghuang <huofenghuang@ubuntu>
2014-12-24 17:09:46 +0800

    create mysql connect

---
+++ frontend/model/MysqlBase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace frontend\model;
+
+/**
+ *
+ * @author huofenghuang
+ *        
+ */
+class MysqlBase {
+	public $handle;//datebase link cache pool
+	
+	/**
+	 */
+	function __construct() {
+		
+		if(empty($this->handle)){
+			$handle = new mysqli('localhost','root','','lottery',3306);
+			if($handle->connect_errno){
+				echo "Fail connect to Mysql:",$handle->connect_errno," ",$handle->connect_error;
+			}
+		}
+		echo $handle->host_info;
+	}
+	
+	/**
+	 */
+	function __destruct() {
+		
+		// TODO - Insert your code here
+	}
+}
+
+?>
--- index.php
+++ index.php
@@ -1,1 +1,4 @@
 <?php
+include_once 'frontend/model/MysqlBase.php';
+use frontend\model\MysqlBase;
+$test = new MysqlBase();
